de61bc85c3f76ab8a43ae52e3a4439abdd8900d1
YosiKimhi <35778740+YosiKimhi@users.noreply.github.com>
2021-05-12 21:21:00 +0300

    fix error

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -7,7 +7,7 @@ const HeaderCartButton = (props) => {
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span classNa>Your Cart</span>
+      <span>Your Cart</span>
       <span className={classes.badge}>3</span>
     </button>
   );
